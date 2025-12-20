274fcb31262f72e5da84f324ebb22e410ba0f563
Carlos Llamas <cmllamas@google.com>
2025-12-20 19:13:51 +0000

    Revert "ANDROID: Compile kselftest_timers_posix_timers code"
    This reverts commit 32c1bffe815c9ed746ce7d84cae07da17db6bca4.
    
    The quoted upstream patch that fixes the test landed as commit
    45c4225c3dcc ("selftests/timers/posix_timers: Add SIG_IGN test"), so
    this can be re-enabled. If there are further issues with the tests they
    need to be addressed upstream or disabled through external config as
    this hack is creating merge issues with upstream patches.
    
    Bug: 369330443
    Bug: 369693249
    Change-Id: I9f4128d955c8df983191b5e78daef58e749fd592
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    GitOrigin-RevId: 9abfadd9cab888366649bf7e69260312f48a94f2

--- tools/testing/selftests/kselftest_harness.h
+++ tools/testing/selftests/kselftest_harness.h
@@ -70,12 +70,6 @@
 
 #include "kselftest.h"
 
-static inline void __kselftest_memset_safe(void *s, int c, size_t n)
-{
-	if (n > 0)
-		memset(s, c, n);
-}
-
 #define TEST_TIMEOUT_DEFAULT 30
 
 /* Utilities exposed to the test definitions */
@@ -422,7 +416,7 @@ static inline void __kselftest_memset_safe(void *s, int c, size_t n)
 				self = mmap(NULL, sizeof(*self), PROT_READ | PROT_WRITE, \
 					MAP_SHARED | MAP_ANONYMOUS, -1, 0); \
 			} else { \
-				__kselftest_memset_safe(&self_private, 0, sizeof(self_private)); \
+				memset(&self_private, 0, sizeof(self_private)); \
 				self = &self_private; \
 			} \
 		} \
