3821e5f22893107fdc7a62fd7c6a5fa6b649885e
Carlos Llamas <cmllamas@google.com>
2026-03-03 22:41:52 +0000

    Merge 0c00ed308d05 ("Merge tag 'for-7.0/block-20260206' of git://git.kernel.org/pub/scm/linux/kernel/git/axboe/linux") into android-mainline
    Steps on the way to v7.0-rc1
    
    Resolves conflicts in:
    	block/blk-crypto-fallback.c
    	include/linux/blk-crypto.h
    
    Cc: Eric Biggers <ebiggers@google.com>
    Change-Id: I64b3b9abb524bf5da75003a02063d652987add92
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    GitOrigin-RevId: 536420681c8c32ff6db35f85fa353fb1e534feea

--- tools/testing/selftests/ublk/test_batch_01.sh
+++ tools/testing/selftests/ublk/test_batch_01.sh
@@ -5,16 +5,16 @@
 
 ERR_CODE=0
 
-if ! _have_feature "AUTO_BUF_REG"; then
+if ! _have_feature "BATCH_IO"; then
 	exit "$UBLK_SKIP_CODE"
 fi
 
-_prep_test "generic" "test UBLK_F_AUTO_BUF_REG"
+_prep_test "generic" "test basic function of UBLK_F_BATCH_IO"
 
 _create_backfile 0 256M
 _create_backfile 1 256M
 
-dev_id=$(_add_ublk_dev -t loop -q 2 --auto_zc "${UBLK_BACKFILES[0]}")
+dev_id=$(_add_ublk_dev -t loop -q 2 -b "${UBLK_BACKFILES[0]}")
 _check_add_dev $TID $?
 
 if ! _mkfs_mount_test /dev/ublkb"${dev_id}"; then
@@ -22,7 +22,7 @@ if ! _mkfs_mount_test /dev/ublkb"${dev_id}"; then
 	_show_result $TID 255
 fi
 
-dev_id=$(_add_ublk_dev -t stripe --auto_zc "${UBLK_BACKFILES[0]}" "${UBLK_BACKFILES[1]}")
+dev_id=$(_add_ublk_dev -t stripe -b --auto_zc "${UBLK_BACKFILES[0]}" "${UBLK_BACKFILES[1]}")
 _check_add_dev $TID $?
 _mkfs_mount_test /dev/ublkb"${dev_id}"
 ERR_CODE=$?
